1cc53f1e5414ce566fe2774943b175bc5f12b422
outlet13f <dsjang@hndsolution.com>
2020-01-22 14:45:39 +0900

    20200122_1445

--- battery/app/src/main/java/com/hndsolution/battery/ui/login/LoginViewModel.java
+++ battery/app/src/main/java/com/hndsolution/battery/ui/login/LoginViewModel.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel;
 
 import android.util.Patterns;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.hndsolution.battery.data.LoginRepository;
 import com.hndsolution.battery.data.Result;
 import com.hndsolution.battery.data.model.LoggedInUser;
@@ -16,6 +17,8 @@ public class LoginViewModel extends ViewModel {
     private MutableLiveData<LoginFormState> loginFormState = new MutableLiveData<>();
     private MutableLiveData<LoginResult> loginResult = new MutableLiveData<>();
     private LoginRepository loginRepository;
+
+    private FirebaseAuth mAuth;
 
     LoginViewModel(LoginRepository loginRepository) {
         this.loginRepository = loginRepository;
--- battery/app/src/main/java/com/hndsolution/battery/utils/SplashActivity.java
+++ battery/app/src/main/java/com/hndsolution/battery/utils/SplashActivity.java
@@ -12,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.eftimoff.androipathview.PathView;
 import com.hndsolution.battery.MainActivity;
 import com.hndsolution.battery.R;
+import com.hndsolution.battery.ui.login.ChooserActivity;
+import com.hndsolution.battery.ui.login.EmailPasswordActivity;
 import com.hndsolution.battery.ui.login.LoginActivity;
 
 import static com.hndsolution.battery.utils.Constants.USER_TOKEN;
@@ -38,7 +40,10 @@ public class SplashActivity extends AppCompatActivity {
         if (mSharedPreferences.getString(USER_TOKEN, null) != null) {
             i = MainActivity.getStartIntent(SplashActivity.this);
         } else {
-            i = LoginActivity.getStartIntent(SplashActivity.this);
+            //i = MainActivity.getStartIntent(SplashActivity.this);
+           //i = LoginActivity.getStartIntent(SplashActivity.this);
+            // i = EmailPasswordActivity.getStartIntent(SplashActivity.this);
+            i = ChooserActivity.getStartIntent(SplashActivity.this);
         }
 
         // TODO :: check for the user_token here & redirect to corresponding class
